63aac6baa059ff2f1da304dffb75400127ef34c2
molefrog <molefrog@gmail.com>
2016-11-19 00:49:46 +0300

    Init package.json and scaffold React-ready Webpack configuration.

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+'use strict'
+
+// Example webpack configuration with asset fingerprinting in production.
+const webpack = require('webpack')
+const path = require('path')
+
+// Port
+let DEV_SERVER_PORT = 3808
+
+// Envs
+const nodeEnv = process.env.NODE_ENV || process.env.RAILS_ENV || 'development'
+const isProduction = nodeEnv === 'production'
+
+// ---
+// Config
+// ---
+var config = {
+  context: __dirname,
+
+  entry: {
+    'application': [ './src/application.js' ]
+  },
+
+  output: {
+    path: path.join(__dirname, '..', 'public'),
+    publicPath: '/',
+    filename: isProduction ? '[name]-[chunkhash].js' : '[name].js'
+  },
+
+  resolve: {
+    modulesDirectories: [
+      'src',
+      'node_modules'
+    ],
+    extensions: ['', '.webpack.js', '.js', '.jsx']
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(isProduction)
+    })
+  ],
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'standard'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [ 'react-hot-loader/webpack', 'babel' ]
+      },
+      {
+        test: /\.scss$/,
+        loaders: [ 'style', 'css', 'sass' ]
+      }
+    ]
+  }
+}
+
+// ---
+// Production config
+// ---
+if (isProduction) {
+  config.plugins.push(
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: { warnings: false },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production') }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+  )
+} else {
+  config.entry.application.unshift('webpack/hot/dev-server')
+  config.entry.application.unshift('webpack-dev-server/client?http://localhost:' + String(DEV_SERVER_PORT))
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+
+  config.devServer = {
+    hot: true,
+    port: DEV_SERVER_PORT,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    contentBase: './public'
+  }
+
+  // config.output.publicPath = '//localhost:' + DEV_SERVER_PORT + '/webpack/'
+  // Source maps
+  config.devtool = 'cheap-module-eval-source-map'
+}
+
+module.exports = config
