843e8ac967421d660f73ee38c047240e82ed9c4c
molefrog <molefrog@gmail.com>
2016-12-09 21:04:19 +0300

    Button component can be togglable now.

--- src/ui/button.js
+++ src/ui/button.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 import './button.scss'
 
-const Button = ({ icon, children, onClick }) =>
-  <button className='button' onClick={onClick}>
+const Button = ({ icon, children, onClick, checked }) =>
+  <button className={`button ${checked ? 'button--checked' : ''}`}
+    onClick={onClick}>
     {icon && <span className='button__icon'>{icon}</span>}
     {children}
   </button>
