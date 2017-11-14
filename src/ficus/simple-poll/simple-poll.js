6a9a4bd87a1e7ed6ccb1d2843c875f6c9d6027c7
Alexey Taktarov <molefrog@gmail.com>
2017-11-14 16:34:54 +0300

    Tweak SimplePoll styles: add BarCount, change ficus link.

--- src/ficus/simple-poll/simple-poll.js
+++ src/ficus/simple-poll/simple-poll.js
@@ -1,73 +1,96 @@
 import React, { Component } from 'react'
+import _ from 'lodash'
 
-const safeFraction = (a, b) => !b ? 0.0 : (a / b)
-const dummyI18n = (text, ...rest) => text
+const safeFraction = (a, b) => (!b ? 0.0 : a / b)
+
+const BarToolip = ({ votes, percent }) => (
+  <div className="simple-poll__bar-tooltip">
+    <div className="simple-poll__bar-tooltip-inner">
+      {votes || 0} · {(percent || 0).toFixed(1)}%
+    </div>
+  </div>
+)
+
+const Bar = ({ width, height, bar }) => {
+  return (
+    <div style={{ width: `${width}%` }} className="simple-poll__bar">
+      <BarToolip votes={bar.votes} percent={bar.weight * 100.0} />
+
+      <div
+        className="simple-poll__bar-progress"
+        style={{
+          backgroundColor: bar.color,
+          height: `${height}%`
+        }}
+      />
+    </div>
+  )
+}
 
 class SimplePoll extends Component {
-  render () {
-    const i18n = this.props.i18n || dummyI18n
-    const subHeader = `${this.getVotersCount()} votes on ` ||
-      i18n('%voters on ', {
-        voters: i18n('total %0 voters', this.getVotersCount())
-      })
+  getBarHeight(value, max) {
+    const step = 10
+
+    const currentLimit = step * Math.ceil(max / step)
+    return Math.max(1, 100.0 * safeFraction(value, currentLimit))
+  }
+
+  render() {
+    const subHeader = `${this.getVotersCount()} votes on`
 
     const { poll } = this.props
 
     const bars = this.getChartData()
-    const maxWeight = Math.min(0.4, Math.max(...bars.map(b => b.weight)))
 
-    const barWidth = `${100.0 / bars.length}%`
+    const maxVotes = _.max(bars.map(b => b.votes))
+    const barWidth = 100.0 / bars.length
 
     return (
-      <div className='simple-poll'>
-        <div className='simple-poll__header'>
-          {poll.title}
-        </div>
-        <div className='simple-poll__subheader'>
-          {subHeader}
-          <span className='simple-poll__subheader-link'>
-            {poll.url}
-          </span>
-        </div>
-
-        <div className='simple-poll__votes'>
-
-          <div className='simple-poll__bars'>
-            {bars.map(bar => (
-              <div style={{ width: barWidth }}
-                key={bar.id} className='simple-poll__bar'>
-
-                <div className='simple-poll__bar-progress'
-                  style={{
-                    backgroundColor: bar.color,
-                    height: `${Math.max(1, 0.8 * 100 * safeFraction(bar.weight, maxWeight))}%`
-                  }} />
-              </div>)
-            )}
+      <div className="simple-poll">
+        <div className="simple-poll__subheader">
+          <div className="simple-poll__subheader-inner">
+            {subHeader}
+            <span className="simple-poll__subheader-link">{poll.url}</span>
           </div>
 
-          <div className='simple-poll__labels'>
-            {bars.map(bar => (
-              <div style={{ width: barWidth }}
-                className='simple-poll__label-col'
-                key={bar.id}>
+          <div className="simple-poll__total">{this.getVotersCount()}</div>
+        </div>
 
-                <div className='simple-poll__label'>
-                  {bar.label}
-                </div>
-              </div>)
-            )}
+        <div className="simple-poll__header">{poll.title}</div>
+
+        <div className="simple-poll__votes">
+          <div className="simple-poll__bars">
+            {bars.map(bar => (
+              <Bar
+                bar={bar}
+                key={bar.id}
+                width={barWidth}
+                height={this.getBarHeight(bar.votes, maxVotes)}
+              />
+            ))}
+          </div>
+
+          <div className="simple-poll__labels">
+            {bars.map(bar => (
+              <div
+                style={{ width: `${barWidth}%` }}
+                className="simple-poll__label-col"
+                key={bar.id}
+              >
+                <div className="simple-poll__label">{bar.label}</div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     )
   }
 
-  getVotersCount () {
+  getVotersCount() {
     return this.props.poll.votersCount
   }
 
-  getChartData () {
+  getChartData() {
     const { poll } = this.props
     if (!poll) return []
 
@@ -77,7 +100,8 @@ class SimplePoll extends Component {
       return {
         ...choice,
         label: choice.text,
-        weight: result.percent
+        weight: result.percent,
+        votes: result.votes
       }
     })
   }
--- src/slides/poll-slides.js
+++ src/slides/poll-slides.js
@@ -10,32 +10,47 @@ import Button from 'ui/button'
 
 import './poll-slides.scss'
 
-const makeUid = () => (Math.random() * 0xffffff | 0)
+const makeUid = () => (Math.random() * 0xffffff) | 0
 
 const initialPoll = {
   title: 'What is the best album of 2016?',
-  url: 'otts.ficus.io',
+  url: 'ficus.io',
   choices: [
-    { id: 'a1', color: 'rgba(61, 118, 230, 1.00)', text: 'Chance The Rapper – Coloring Book' },
+    {
+      id: 'a1',
+      color: 'rgba(61, 118, 230, 1.00)',
+      text: 'Chance The Rapper – Coloring Book'
+    },
     { id: 'a2', color: 'rgba(241, 196, 15, 1.00)', text: 'Beyoncé – Lemonade' },
-    { id: 'a3', color: 'rgba(230, 126, 34, 1.00)', text: 'Frank Ocean – Blonde  ' },
+    {
+      id: 'a3',
+      color: 'rgba(230, 126, 34, 1.00)',
+      text: 'Frank Ocean – Blonde  '
+    },
     { id: 'a4', color: 'rgba(231, 76, 60, 1.00)', text: 'David Bowie – Star' },
-    { id: 'a5', color: 'rgba(142, 68, 173, 1.00)', text: 'Kanye West – The Life Of Pablo' }
+    {
+      id: 'a5',
+      color: 'rgba(142, 68, 173, 1.00)',
+      text: 'Kanye West – The Life Of Pablo'
+    }
   ],
   voters: []
 }
 
-const makePollPayload = (poll) => {
+const makePollPayload = poll => {
   const votersCount = poll.voters.length
   const results = {}
-  poll.choices.forEach(ch => { results[ch.id] = { votes: 0 } })
+  poll.choices.forEach(ch => {
+    results[ch.id] = { votes: 0 }
+  })
   poll.voters.forEach(v => {
     results[v.choice].votes++
   })
 
   poll.choices.forEach(ch => {
     results[ch.id].percent = votersCount
-      ? (results[ch.id].votes / votersCount) : 0
+      ? results[ch.id].votes / votersCount
+      : 0
   })
 
   return {
@@ -45,7 +60,7 @@ const makePollPayload = (poll) => {
   }
 }
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)]
+const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const generateVoter = (poll, choices) => {
   const ch = choices || poll.choices
@@ -57,7 +72,8 @@ const generateVoter = (poll, choices) => {
 }
 
 const addVotes = (poll, amount) => {
-  const newVoters = Array(amount).fill()
+  const newVoters = Array(amount)
+    .fill()
     .map(() => generateVoter(poll))
 
   return {
@@ -77,144 +93,188 @@ const changeVotes = (poll, percent) => {
     ...poll,
     voters: poll.voters.map(v => ({
       ...v,
-      choice: (Math.random() <= percent ? sample(choices).id : v.choice)
+      choice: Math.random() <= percent ? sample(choices).id : v.choice
     }))
   }
 }
 
 class PollControls extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       poll: addVotes(initialPoll, 80)
     }
   }
 
-  changeVotes (percent) {
+  changeVotes(percent) {
     this.setState({
       poll: changeVotes(this.state.poll, percent)
     })
   }
 
-  addVotes (amount = 1) {
+  addVotes(amount = 1) {
     this.setState({
       poll: addVotes(this.state.poll, amount)
     })
   }
 
-  render () {
+  render() {
     return (
-      <div className='poll-slide__wrap'>
-        <div className='poll-slide__content'>
+      <div className="poll-slide__wrap">
+        <div className="poll-slide__content">
           {this.props.children(makePollPayload(this.state.poll))}
         </div>
 
-        <div className='poll-slide__controls'>
-          <Button icon='👏' onClick={() => this.changeVotes(0.2)}>поменять 20%</Button>
-          <Button icon='👏' onClick={() => this.changeVotes(0.8)}>поменять 80%</Button>
-          <Button icon='⭐️' onClick={() => this.addVotes(1)}>+1 голос</Button>
-          <Button icon='⭐' onClick={() => this.addVotes(10)}>+10 голосов</Button>
+        <div className="poll-slide__controls">
+          <Button icon="👏" onClick={() => this.changeVotes(0.2)}>
+            поменять 20%
+          </Button>
+          <Button icon="👏" onClick={() => this.changeVotes(0.8)}>
+            поменять 80%
+          </Button>
+          <Button icon="⭐️" onClick={() => this.addVotes(1)}>
+            +1 голос
+          </Button>
+          <Button icon="⭐" onClick={() => this.addVotes(10)}>
+            +10 голосов
+          </Button>
         </div>
       </div>
     )
   }
 }
 
-const PollZoom = ({ zoom, children, width, height }) =>
-  <div className='poll-zoom__poll'
-    style={{ width: width * zoom, height: height * zoom }}>
-    <div className='poll-zoom__poll-wrap'
-      style={{ transform: `scale(${zoom}, ${zoom})` }}>
+const PollZoom = ({ zoom, children, width, height }) => (
+  <div
+    className="poll-zoom__poll"
+    style={{ width: width * zoom, height: height * zoom }}
+  >
+    <div
+      className="poll-zoom__poll-wrap"
+      style={{ transform: `scale(${zoom}, ${zoom})` }}
+    >
       {children}
     </div>
   </div>
+)
 
 export class CloudPollSlide extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = { alphaTime: 1.4 }
   }
 
-  toggleSpeed () {
+  toggleSpeed() {
     this.setState({ alphaTime: this.state.alphaTime === 1.4 ? 4.0 : 1.4 })
   }
 
-  render () {
+  render() {
     const width = 1100
     const height = 660
 
     const alphaTime = this.state.alphaTime
 
     return (
-      <Slide {...this.props} extraClass='poll-slide'>
+      <Slide {...this.props} extraClass="poll-slide">
         <PollControls>
-          {poll =>
+          {poll => (
             <PollZoom width={width} height={height} zoom={0.65}>
-              <CloudPoll width={width} height={height}
+              <CloudPoll
+                width={width}
+                height={height}
                 alphaTime={alphaTime}
-                offsetContainerClass='poll-zoom__poll' poll={poll} />
-            </PollZoom>}
+                offsetContainerClass="poll-zoom__poll"
+                poll={poll}
+              />
+            </PollZoom>
+          )}
         </PollControls>
 
-        <div className='poll-slide__speed'>
-          <Button icon='⭐' checked={alphaTime === 4.0} onClick={() => this.toggleSpeed()}>
+        <div className="poll-slide__speed">
+          <Button
+            icon="⭐"
+            checked={alphaTime === 4.0}
+            onClick={() => this.toggleSpeed()}
+          >
             {alphaTime === 1.4 ? 'нормально' : 'медленно'}
           </Button>
         </div>
-
-      </Slide>)
+      </Slide>
+    )
   }
 }
 
 export class BubblePollSlide extends Component {
-  render () {
+  render() {
     const width = 1024
     const height = 768
 
     return (
-      <Slide {...this.props} extraClass='poll-slide'>
+      <Slide {...this.props} extraClass="poll-slide">
         <PollControls>
-          {poll =>
+          {poll => (
             <PollZoom width={width} height={height} zoom={0.65}>
-              <BubblePoll width={width} height={height}
-                offsetContainerClass='poll-zoom__poll' poll={poll} />
-            </PollZoom>}
+              <BubblePoll
+                width={width}
+                height={height}
+                offsetContainerClass="poll-zoom__poll"
+                poll={poll}
+              />
+            </PollZoom>
+          )}
         </PollControls>
-      </Slide>)
+      </Slide>
+    )
   }
 }
 
-export const PollsSlide = (props) => {
+export const PollsSlide = props => {
   const width = 1024
   const height = 768
 
   return (
-    <Slide {...props} extraClass='poll-slide'>
+    <Slide {...props} extraClass="poll-slide">
       <PollControls>
-        {poll =>
-          <div className='poll-slide__polls'>
+        {poll => (
+          <div className="poll-slide__polls">
             <PollZoom width={width} height={height} zoom={0.3}>
-              <CloudPoll width={width} height={height}
-                offsetContainerClass='poll-zoom__poll' poll={poll} />
+              <CloudPoll
+                width={width}
+                height={height}
+                offsetContainerClass="poll-zoom__poll"
+                poll={poll}
+              />
             </PollZoom>
 
             <PollZoom width={width} height={height} zoom={0.3}>
-              <SimplePoll width={width} height={height}
-                offsetContainerClass='poll-zoom__poll' poll={poll} />
+              <SimplePoll
+                width={width}
+                height={height}
+                offsetContainerClass="poll-zoom__poll"
+                poll={poll}
+              />
             </PollZoom>
 
             <PollZoom width={width} height={height} zoom={0.3}>
-              <BubblePoll width={width} height={height}
-                offsetContainerClass='poll-zoom__poll' poll={poll} />
+              <BubblePoll
+                width={width}
+                height={height}
+                offsetContainerClass="poll-zoom__poll"
+                poll={poll}
+              />
             </PollZoom>
 
             <PollZoom width={width} height={height} zoom={0.3}>
-              <ClassicPoll width={width} height={height}
-                offsetContainerClass='poll-zoom__poll' poll={poll} />
+              <ClassicPoll
+                width={width}
+                height={height}
+                offsetContainerClass="poll-zoom__poll"
+                poll={poll}
+              />
             </PollZoom>
           </div>
-
-        }
+        )}
       </PollControls>
-    </Slide>)
+    </Slide>
+  )
 }
