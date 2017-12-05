8001c5eb0022c59e4ddc690fb03620542a638347
Alexey Taktarov <molefrog@gmail.com>
2017-12-05 15:32:46 +0300

    More slides.

--- src/presentation.js
+++ src/presentation.js
@@ -1,14 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
 // Presentation pieces
 import { Presentation, Slide } from 'presa'
-import {
-  Title,
-  Caption,
-  FrameBackground,
-  Code,
-  FigureCaption
-} from 'presa/blocks'
+import { Title, Caption, Code } from 'presa/blocks'
+
+import FigureCaption from 'blocks/figure-caption'
+import FrameBackground from 'blocks/frame-background'
+import Layout from 'blocks/layout'
 
 // Interactive slides
 import TransistorSlide from 'slides/transistor'
@@ -31,20 +30,50 @@ import {
 import DialogSlide from 'slides/enter-exit'
 
 const DirtyAnimations = props => (
-  <Presentation name="Грязные анимации в мире состояний">
+  <Presentation
+    name="Анимации в мире состояний"
+    tableOfContents
+    theme={{
+      slide: { fontScale: 1.414 }
+    }}
+  >
     {/* Введение */}
-    <Slide name="Титульный слайд" background="black" centered>
-      <Title color="white">Грязные анимации в мире состояний</Title>
-      <Caption color="white">
-        Алексей Тактаров<br />HolyJS Moscow 2017
-      </Caption>
+    <Slide
+      name="Титульный слайд"
+      background="/images/gifs/abstract.gif"
+      fade={0.5}
+    >
+      <Layout>
+        <DeckTitle color="white">
+          Анимации ↝<br />в мире состояний
+        </DeckTitle>
+
+        <Contacts>
+          <Author>
+            Алексей Тактаров<br />HolyJS Moscow 2017
+          </Author>
+
+          <TwitterHandle>@mlfrg · molefrog.com</TwitterHandle>
+        </Contacts>
+      </Layout>
     </Slide>
 
-    <Slide background="/images/vitruvius-pitch.png" />
-    <Slide background="/images/vitruvius-book.png" />
-    <Slide background="/images/vitruvius-rules.png" />
-    <Slide background="/images/rams-radio.png" />
-    <Slide background="/images/gifs/apps.gif" fade={0.2} centered />
+    <Slide
+      name="Витрувий — один из первых архитекторов-теоретиков"
+      background="/images/vitruvius-pitch.jpg"
+    />
+    <Slide
+      name="Витрувий написал труд по архитектуре"
+      background="/images/vitruvius-book.jpg"
+    />
+    <Slide name="Три качества" background="/images/vitruvius-rules.png" />
+    <Slide name="Применимы везде" background="/images/rams-radio.jpg" />
+    <Slide
+      name="В чем наша миссия?"
+      background="/images/gifs/apps.gif"
+      fade={0.2}
+      centered
+    />
 
     {/* Про что доклад? */}
     <Slide
@@ -63,9 +92,12 @@ const DirtyAnimations = props => (
       name="Но наивная реализация некорректна"
       comparedMethod="naive"
     />
-    <RafTimestampSlide />
-    <RafDeltaSlide />
-    <TimelineComparisonSlide comparedMethod="raf" />
+    <RafTimestampSlide name="rAF принимает параметр-время" />
+    <RafDeltaSlide name="Учитываем дельту в анимации" />
+    <TimelineComparisonSlide
+      name="Пропуск кадров, но анимация корректна"
+      comparedMethod="raf"
+    />
 
     <Slide
       name={
@@ -112,27 +144,30 @@ const DirtyAnimations = props => (
       }
       motionEnabled
     />
-    <ReactMotionCodeSlide />
+    <ReactMotionCodeSlide name="Function-as-a-Prop" />
 
     <MotionGhostSlide
       name="Как React Motion работает с состояниями."
       centered
     />
 
-    <Slide background="/images/react-motions-cons.png" />
+    <Slide
+      name="React Motion не всегда подходит"
+      background="/images/react-motions-cons.png"
+    />
 
     {/* Делаем кастомную анимацию на примере */}
     <Slide
       fade={0.4}
       centered
-      name="React Lifecycle Hooks"
       background="/images/gifs/factory.gif"
+      name="Грязные анимации"
     >
       <Title color="white">Как делать «грязные» анимации</Title>
       <Caption color="white">На примере диалогового окна</Caption>
     </Slide>
 
-    <Slide centered>
+    <Slide name="Анимации на входе" centered>
       <Code>{`
 class Dialog extends Component {
   componentDidMount() {
@@ -151,7 +186,7 @@ class Dialog extends Component {
       </FigureCaption>
     </Slide>
 
-    <Slide centered>
+    <Slide name="Отмена анимации" centered>
       <Code>{`class Dialog extends Component {
   componentDidMount() {
     const node = findDOMNode(this)
@@ -170,9 +205,9 @@ class Dialog extends Component {
       </FigureCaption>
     </Slide>
 
-    <DialogSlide example="enter" />
+    <DialogSlide name="Диалоговое окно: вход" example="enter" />
 
-    <Slide centered>
+    <Slide name="Проблемы с выходом" centered>
       <Code>{`
 <div>
   {this.state.showDialog && <Dialog />}
@@ -183,9 +218,14 @@ class Dialog extends Component {
       </FigureCaption>
     </Slide>
 
-    <Slide background="/images/state-map.png" />
+    <Slide name="Карта состояний" centered>
+      <img src="/images/state-map.png" width="800" />
+      <FigureCaption>
+        Карта состояний компонента <code>Animated</code>
+      </FigureCaption>
+    </Slide>
 
-    <Slide centered>
+    <Slide name="Интерфейс Animated" centered>
       <Code>{`
 <Animated>
   {this.state.showDialog && <Dialog />}
@@ -196,7 +236,7 @@ class Dialog extends Component {
       </FigureCaption>
     </Slide>
 
-    <Slide centered>
+    <Slide name="Во что превращается JSX" centered>
       <Code fontSize={22}>{`const element = <Dialog size="medium" />
 
 // => { type: Dialog, props: { size: 'medium' }, ... }
@@ -205,9 +245,12 @@ const element = React.createElement(Dialog, { size: 'medium' })`}</Code>
       <FigureCaption>Что скрывается за JSX?</FigureCaption>
     </Slide>
 
-    <Slide background="/images/dan-article.png" />
+    <Slide
+      name="Тонкости хорошо раскрыты в статье"
+      background="/images/dan-article.png"
+    />
 
-    <Slide centered>
+    <Slide name="Animated — простейший автомат" centered>
       <Code>{`
   componentWillReceiveProps(nextProps) {
     // Exit transition
@@ -215,11 +258,11 @@ const element = React.createElement(Dialog, { size: 'medium' })`}</Code>
       return this.transitionState(st.EXITING,
         { children: this.props.children })
     }
+  }
 
-    // Enter transition
-    if (!this.props.children && nextProps.children) {
-      return this.transitionState(st.ENTERING)
-    }
+  transitionState(transitionTo, opt = {}) {
+    // .. FSM logic ..
+    // Wait for \`this._content.animateExit()\`
   }
 `}</Code>
       <FigureCaption>
@@ -227,11 +270,11 @@ const element = React.createElement(Dialog, { size: 'medium' })`}</Code>
       </FigureCaption>
     </Slide>
 
-    <DialogSlide example="exit" />
+    <DialogSlide name="Диалог с входом и выходом" example="exit" />
 
     <CloudPollSlide name="Паттерн Change Detection на примере" />
 
-    <Slide centered>
+    <Slide name="Перехват ответственности" centered>
       <Code fontSize={20}>{`render() {
   return <canvas />
 }
@@ -266,8 +309,6 @@ componentWillReceiveProps(nextProps) {
       background="/images/state-changes.png"
     />
 
-    <Slide background="/images/demo-wip.png" />
-
     <Slide name="Redux Actuator позволяет слать события компонентам через Redux store">
       <TalkingHeads />
     </Slide>
@@ -275,8 +316,31 @@ componentWillReceiveProps(nextProps) {
     <Slide name="Redux Actuator поддерживает механизм каналов">
       <InternationalHeads />
     </Slide>
-    <Slide name="Резюме и ресурсы" />
   </Presentation>
 )
 
 export default DirtyAnimations
+
+const DeckTitle = styled(Title)`
+  line-height: 0.95;
+  margin-top: 80px;
+`
+
+const Contacts = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 200px;
+  color: white;
+  font-weight: bold;
+  font-size: 24px;
+  align-items: flex-end;
+`
+
+const Author = styled.div`
+  border-bottom: 4px solid white;
+  padding-top: 10px;
+`
+
+const TwitterHandle = styled.div`
+  font-size: 20px;
+`
