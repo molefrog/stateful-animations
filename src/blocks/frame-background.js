c2ee826afe8c0819dda5d37ecd7a88401f77c6a5
Alexey Taktarov <molefrog@gmail.com>
2017-12-05 15:32:21 +0300

    Add custom blocks to the project.

---
+++ src/blocks/frame-background.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const FrameBackground = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  overflow: hidden;
+  border: none;
+`
+
+export default FrameBackground
---
+++ src/blocks/layout.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+
+  padding: 60px 80px;
+  box-sizing: border-box;
+`
+
+export default Layout
